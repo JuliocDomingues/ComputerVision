60d6a1787fde876f905238fe7b743d11e8f8769e
Julio César Domingues dos Santos - Estágio SST <estagio.sst17@sesims.com.br>
2022-02-17 14:15:55 -0400

    Code refactoring

--- FaceRecognition/FaceRecognition/Form1.cs
+++ FaceRecognition/FaceRecognition/Form1.cs
@@ -1,19 +1,13 @@
 ﻿using System;
-using System.IO;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 using OpenCvSharp;
 using OpenCvSharp.Extensions;
 using FaceRecognitionDotNet;
-using Emgu.CV;
-using Emgu.CV.Structure;
-
+using CenterFaceDotNet;
+using NcnnDotNet.OpenCV;
+using System.Drawing;
 
 namespace FaceDetection
 {
@@ -23,15 +17,27 @@ namespace FaceDetection
 
         #region Variables
         private VideoCapture _capture = null;
-        OpenCvSharp.Mat _frame = new OpenCvSharp.Mat();
         private bool detectFace = false;
         private bool addPerson = false;
         private bool savePerson = false;
-        private Emgu.CV.CascadeClassifier mCascadeClassifier;
-        private OpenCvSharp.Mat smallFace = new OpenCvSharp.Mat();
-        private FaceRecognitionDotNet.FaceRecognition mFaceRecognition;
-        private string path;
-        private string git;
+        string path;
+        OpenCvSharp.Mat image;
+        OpenCvSharp.Mat currentFrame;
+        Bitmap smallImage;
+
+        //CenterFaceParameter param = new CenterFaceParameter
+        //{
+        //    BinFilePath = Directory.GetCurrentDirectory() + @"\centerface.bin",
+        //    ParamFilePath = Directory.GetCurrentDirectory() + @"\centerface.param"
+        //};
+
+        private CenterFaceDotNet.CenterFace _centerFace = CenterFace.Create(new CenterFaceParameter
+        {
+            BinFilePath = Directory.GetCurrentDirectory() + @"\centerface.bin",
+            ParamFilePath = Directory.GetCurrentDirectory() + @"\centerface.param"
+        });
+
+
         #endregion
 
         #region Constructor
@@ -50,7 +56,7 @@ namespace FaceDetection
         private void btnCapture_Click(object sender, EventArgs e)
         {
             _capture = new VideoCapture(0);
-            mCascadeClassifier = new Emgu.CV.CascadeClassifier("haarcascade_frontalface_alt.xml");
+            //mCascadeClassifier = new Emgu.CV.CascadeClassifier("haarcascade_frontalface_alt.xml");
             Application.Idle += ProcessFrame;
         }
 
@@ -73,72 +79,128 @@ namespace FaceDetection
         #region Processing Functions
         private void ProcessFrame(object sender, EventArgs e)
         {
-            _frame = GetFrame(_capture, 1.0);
-            _capture.Retrieve(_frame, 0);
-
-            Bitmap bitmap = new Bitmap(MatToBitmap(_frame));
+            image = GetFrame(_capture, 1.0);
 
             if (detectFace)
-            {   
-                Rectangle[] rectangles = DetectFaces(bitmap);
+            {
+                DetectFaces(image);
 
-                if(rectangles.Count() > 0)
+                if (addPerson)
                 {
-                    foreach(Rectangle rect in rectangles)
-                    { 
-                        if (addPerson)
-                        {
-                            imgFace.Image = ResizeBitmap(CropBitmap(bitmap, rect), imgFace.Width, imgFace.Height);
-                            if (savePerson)
-                            {
-                                path = Directory.GetCurrentDirectory() + @"\SavedImages";
+                    imgFace.Image = smallImage;
 
-                                if (!Directory.Exists(path))
-                                    Directory.CreateDirectory(path);
+                    if (savePerson)
+                    {
+                        path = Directory.GetCurrentDirectory() + @"\SavedImages";
 
-                                imgFace.Image.Save(path + @"\" + txtName.Text + "_" + DateTime.Now.ToString("dddd, dd MMMM yyyy HH-mm-ss") + ".jpg");
-                                savePerson = false;
-                                addPerson = false;
-                                imgFace.Image = null;
-                            }
-                        }
-                        else
-                        {
-                            using (Graphics graphics = Graphics.FromImage(bitmap))
-                            {
-                                using (Pen pen = new Pen(Color.Red, 1))
-                                {
-                                    graphics.DrawRectangle(pen, rect);
-                                }
-                            }
-                        }
+                        if (!Directory.Exists(path))
+                            Directory.CreateDirectory(path);
+
+                        imgFace.Image.Save(path + @"\" + txtName.Text + "_" + DateTime.Now.ToString("dddd, dd MMMM yyyy HH-mm-ss") + ".jpg");
+                        savePerson = false;
+                        addPerson = false;
+                        imgFace.Image = null;
                     }
                 }
             }
-            imgCapture.Image = bitmap;
+
+            imgCapture.Image = image.ToBitmap();
+            #region Stopped
+            //Bitmap bitmap = new Bitmap(MatToBitmap(_frame));
+
+            //if (detectFace)
+            //{   
+            //    Rectangle[] rectangles = DetectFaces(bitmap);
+
+            //    if(rectangles.Count() > 0)
+            //    {
+            //        foreach(Rectangle rect in rectangles)
+            //        { 
+            //            if (addPerson)
+            //            {
+            //                imgFace.Image = ResizeBitmap(CropBitmap(bitmap, rect), imgFace.Width, imgFace.Height);
+            //                if (savePerson)
+            //                {
+            //                    path = Directory.GetCurrentDirectory() + @"\SavedImages";
+
+            //                    if (!Directory.Exists(path))
+            //                        Directory.CreateDirectory(path);
+
+            //                    imgFace.Image.Save(path + @"\" + txtName.Text + "_" + DateTime.Now.ToString("dddd, dd MMMM yyyy HH-mm-ss") + ".jpg");
+            //                    savePerson = false;
+            //                    addPerson = false;
+            //                    imgFace.Image = null;
+            //                }
+            //            }
+            //            else
+            //            {
+            //                using (Graphics graphics = Graphics.FromImage(bitmap))
+            //                {
+            //                    using (Pen pen = new Pen(Color.Red, 1))
+            //                    {
+            //                        graphics.DrawRectangle(pen, rect);
+            //                    }
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
+            #endregion
         }
 
-        private Rectangle[] DetectFaces(Bitmap currentFrame)
+        private void DetectFaces(OpenCvSharp.Mat image)
         {
-            Image<Bgr, Byte> grayImage = new Image<Bgr, Byte>(currentFrame);
-            Rectangle[] rectangles = mCascadeClassifier.DetectMultiScale(grayImage, 1.1, 3);
+            #region Variables
+            //var binPath = Directory.GetCurrentDirectory() + @"\centerface.bin";
+            //var paramPath = Directory.GetCurrentDirectory() + @"\centerface.param";
 
-            return rectangles;
-        }
 
-        #endregion
+            //var param = new CenterFaceParameter
+            //{
+            //    BinFilePath = binPath,
+            //    ParamFilePath = paramPath
+            //};
+            #endregion
 
-        #region Helper Functions
+            //using (var centerFace = CenterFace.Create(param))
+            //{
+            var inMat = NcnnDotNet.Mat.FromPixels(image.Data, NcnnDotNet.PixelType.Bgr2Gray, image.Cols, image.Rows);
+            var faceInfos = _centerFace.Detect(inMat, image.Cols, image.Rows).ToArray();
 
-        public Bitmap ResizeBitmap(Bitmap bmp, int width, int height)
-        {
-            Bitmap result = new Bitmap(width, height);
-            using (Graphics g = Graphics.FromImage(result))
-            {
-                g.DrawImage(bmp, 0, 0, width, height);
+            if (!addPerson) 
+            { 
+                foreach (FaceInfo face in faceInfos)
+                {
+                    var pt1 = new OpenCvSharp.Point(face.X1, face.Y1);
+                    var pt2 = new OpenCvSharp.Point(face.X2, face.Y2);
+                    OpenCvSharp.Cv2.Rectangle(image, pt1, pt2, new Scalar(0, 255, 0), 2);
+
+                    for (var j = 0; j < 5; j++)
+                    {
+                        var center = new OpenCvSharp.Point(face.Landmarks[2 * j], face.Landmarks[2 * j + 1]);
+                        OpenCvSharp.Cv2.Circle(image, center, 2, new Scalar(255, 255, 0), 2);
+                    }
+                }
             }
+            else
+            {
+                foreach (FaceInfo face in faceInfos)
+                {
+                    var pt1 = new System.Drawing.Point((int)face.X1, (int)face.Y1);
+                    var size = new System.Drawing.Size((int)(face.X2 - face.X1), (int)(face.Y2 - face.Y1));
 
-            return result;
+                    smallImage = ResizeBitmap(CropBitmap(image.ToBitmap(), new Rectangle(pt1, size)), imgFace.Width, imgFace.Height);
+                }
+            }
+            //}
+        }
+
+        private static OpenCvSharp.Mat GetFrame(VideoCapture cap, double scalingFactor)
+        {
+            OpenCvSharp.Mat frame = new OpenCvSharp.Mat();
+            bool ret = cap.Read(frame);
+            OpenCvSharp.Cv2.Resize(frame, frame, new OpenCvSharp.Size(), fx: scalingFactor, fy: scalingFactor, interpolation: InterpolationFlags.Area);
+            return frame;
         }
 
         private static Bitmap CropBitmap(Bitmap image, Rectangle rect)
@@ -149,16 +211,15 @@ namespace FaceDetection
             return cropImage;
         }
 
-        private static Bitmap MatToBitmap(OpenCvSharp.Mat mat)
+        public Bitmap ResizeBitmap(Bitmap bmp, int width, int height)
         {
-            return OpenCvSharp.Extensions.BitmapConverter.ToBitmap(mat);
-        }
-        private static OpenCvSharp.Mat GetFrame(VideoCapture cap, double scalingFactor)
-        {
-            OpenCvSharp.Mat frame = new OpenCvSharp.Mat();
-            bool ret = cap.Read(frame);
-            Cv2.Resize(frame, frame, new OpenCvSharp.Size(), fx: scalingFactor, fy: scalingFactor, interpolation: InterpolationFlags.Area);
-            return frame;
+            Bitmap result = new Bitmap(width, height);
+            using (Graphics g = Graphics.FromImage(result))
+            {
+                g.DrawImage(bmp, 0, 0, width, height);
+            }
+
+            return result;
         }
         #endregion
 
