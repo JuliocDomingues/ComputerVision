c369ae49a83411ac541e5195d71f219e701d67b2
Julio César Domingues dos Santos - Estágio SST <estagio.sst17@sesims.com.br>
2022-02-17 08:27:22 -0400

    New Aplication

---
+++ FaceRecognition/FaceRecognition/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace FaceDetection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imgCapture = new System.Windows.Forms.PictureBox();
+            this.btnCapture = new System.Windows.Forms.Button();
+            this.btnDetectFace = new System.Windows.Forms.Button();
+            this.btnAddFace = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.imgFace = new System.Windows.Forms.PictureBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imgCapture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgFace)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgCapture
+            // 
+            this.imgCapture.Location = new System.Drawing.Point(2, 2);
+            this.imgCapture.Name = "imgCapture";
+            this.imgCapture.Size = new System.Drawing.Size(714, 554);
+            this.imgCapture.TabIndex = 0;
+            this.imgCapture.TabStop = false;
+            this.imgCapture.Click += new System.EventHandler(this.imgCapture_Click);
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.Location = new System.Drawing.Point(722, 12);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(206, 30);
+            this.btnCapture.TabIndex = 1;
+            this.btnCapture.Text = "Capture";
+            this.btnCapture.UseVisualStyleBackColor = true;
+            this.btnCapture.Click += new System.EventHandler(this.btnCapture_Click);
+            // 
+            // btnDetectFace
+            // 
+            this.btnDetectFace.Location = new System.Drawing.Point(722, 48);
+            this.btnDetectFace.Name = "btnDetectFace";
+            this.btnDetectFace.Size = new System.Drawing.Size(206, 31);
+            this.btnDetectFace.TabIndex = 2;
+            this.btnDetectFace.Text = "Detect Face";
+            this.btnDetectFace.UseVisualStyleBackColor = true;
+            this.btnDetectFace.Click += new System.EventHandler(this.btnDetectFace_Click);
+            // 
+            // btnAddFace
+            // 
+            this.btnAddFace.Location = new System.Drawing.Point(722, 85);
+            this.btnAddFace.Name = "btnAddFace";
+            this.btnAddFace.Size = new System.Drawing.Size(206, 28);
+            this.btnAddFace.TabIndex = 3;
+            this.btnAddFace.Text = "Add Face";
+            this.btnAddFace.UseVisualStyleBackColor = true;
+            this.btnAddFace.Click += new System.EventHandler(this.btnAddFace_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(722, 270);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(206, 22);
+            this.txtName.TabIndex = 4;
+            // 
+            // imgFace
+            // 
+            this.imgFace.Location = new System.Drawing.Point(722, 119);
+            this.imgFace.Name = "imgFace";
+            this.imgFace.Size = new System.Drawing.Size(206, 145);
+            this.imgFace.TabIndex = 5;
+            this.imgFace.TabStop = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(722, 298);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(206, 27);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save Image";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(940, 559);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.imgFace);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnAddFace);
+            this.Controls.Add(this.btnDetectFace);
+            this.Controls.Add(this.btnCapture);
+            this.Controls.Add(this.imgCapture);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imgCapture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgFace)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imgCapture;
+        private System.Windows.Forms.Button btnCapture;
+        private System.Windows.Forms.Button btnDetectFace;
+        private System.Windows.Forms.Button btnAddFace;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.PictureBox imgFace;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ FaceRecognition/FaceRecognition/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using FaceRecognitionDotNet;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+
+namespace FaceDetection
+{
+    public partial class Form1 : Form
+    {
+        
+
+        #region Variables
+        private VideoCapture _capture = null;
+        OpenCvSharp.Mat _frame = new OpenCvSharp.Mat();
+        private bool detectFace = false;
+        private bool addPerson = false;
+        private bool savePerson = false;
+        private Emgu.CV.CascadeClassifier mCascadeClassifier;
+        private OpenCvSharp.Mat smallFace = new OpenCvSharp.Mat();
+        private FaceRecognitionDotNet.FaceRecognition mFaceRecognition;
+        private string path;
+        private string git;
+        #endregion
+
+        #region Constructor
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Functionalities
+        private void imgCapture_Click(object sender, EventArgs e)
+        {
+            //Main Screen
+        }
+
+        private void btnCapture_Click(object sender, EventArgs e)
+        {
+            _capture = new VideoCapture(0);
+            mCascadeClassifier = new Emgu.CV.CascadeClassifier("haarcascade_frontalface_alt.xml");
+            Application.Idle += ProcessFrame;
+        }
+
+        private void btnDetectFace_Click(object sender, EventArgs e)
+        {
+            detectFace = true;
+        }
+
+        private void btnAddFace_Click(object sender, EventArgs e)
+        {
+            addPerson = true;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            savePerson = true;
+        }
+        #endregion
+
+        #region Processing Functions
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            _frame = GetFrame(_capture, 1.0);
+            _capture.Retrieve(_frame, 0);
+
+            Bitmap bitmap = new Bitmap(MatToBitmap(_frame));
+
+            if (detectFace)
+            {   
+                Rectangle[] rectangles = DetectFaces(bitmap);
+
+                if(rectangles.Count() > 0)
+                {
+                    foreach(Rectangle rect in rectangles)
+                    { 
+                        if (addPerson)
+                        {
+                            imgFace.Image = ResizeBitmap(CropBitmap(bitmap, rect), imgFace.Width, imgFace.Height);
+                            if (savePerson)
+                            {
+                                path = Directory.GetCurrentDirectory() + @"\SavedImages";
+
+                                if (!Directory.Exists(path))
+                                    Directory.CreateDirectory(path);
+
+                                imgFace.Image.Save(path + @"\" + txtName.Text + "_" + DateTime.Now.ToString("dddd, dd MMMM yyyy HH-mm-ss") + ".jpg");
+                                savePerson = false;
+                                addPerson = false;
+                                imgFace.Image = null;
+                            }
+                        }
+                        else
+                        {
+                            using (Graphics graphics = Graphics.FromImage(bitmap))
+                            {
+                                using (Pen pen = new Pen(Color.Red, 1))
+                                {
+                                    graphics.DrawRectangle(pen, rect);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            imgCapture.Image = bitmap;
+        }
+
+        private Rectangle[] DetectFaces(Bitmap currentFrame)
+        {
+            Image<Bgr, Byte> grayImage = new Image<Bgr, Byte>(currentFrame);
+            Rectangle[] rectangles = mCascadeClassifier.DetectMultiScale(grayImage, 1.1, 3);
+
+            return rectangles;
+        }
+
+        #endregion
+
+        #region Helper Functions
+
+        public Bitmap ResizeBitmap(Bitmap bmp, int width, int height)
+        {
+            Bitmap result = new Bitmap(width, height);
+            using (Graphics g = Graphics.FromImage(result))
+            {
+                g.DrawImage(bmp, 0, 0, width, height);
+            }
+
+            return result;
+        }
+
+        private static Bitmap CropBitmap(Bitmap image, Rectangle rect)
+        {
+            Bitmap tmpImage = new Bitmap(image);
+            Bitmap cropImage = tmpImage.Clone(rect, tmpImage.PixelFormat);
+
+            return cropImage;
+        }
+
+        private static Bitmap MatToBitmap(OpenCvSharp.Mat mat)
+        {
+            return OpenCvSharp.Extensions.BitmapConverter.ToBitmap(mat);
+        }
+        private static OpenCvSharp.Mat GetFrame(VideoCapture cap, double scalingFactor)
+        {
+            OpenCvSharp.Mat frame = new OpenCvSharp.Mat();
+            bool ret = cap.Read(frame);
+            Cv2.Resize(frame, frame, new OpenCvSharp.Size(), fx: scalingFactor, fy: scalingFactor, interpolation: InterpolationFlags.Area);
+            return frame;
+        }
+        #endregion
+
+    }
+}
