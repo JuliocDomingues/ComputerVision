57a1e39104125f5822c318421b1ab678d8da1c9a
Julio César Domingues dos Santos - Estágio SST <estagio.sst17@sesims.com.br>
2022-03-04 14:42:06 -0400

    Add Face Distances

--- FaceMatching/FaceMatching/FaceMatching/Services/MatchFaces.cs
+++ FaceMatching/FaceMatching/FaceMatching/Services/MatchFaces.cs
@@ -9,11 +9,13 @@ namespace FaceMatching.Services
 {
     public class MatchFaces
     {
+
         public static bool MatchFace(Bitmap image)
         {
             string[] folders = Directory.GetDirectories(Form1.pathEncoding, "*", SearchOption.AllDirectories);
 
             var imageBit = FaceRecognition.LoadImage(image);
+
             var encodingImage = Form1._FaceRecognition.FaceEncodings(imageBit, model: Model.Cnn).ToArray();
 
             var bf = new BinaryFormatter();
@@ -27,10 +29,29 @@ namespace FaceMatching.Services
                     using (FileStream stream = new FileStream(file, FileMode.Open))
                     {
                         var facesEncodings = (System.Collections.Generic.IEnumerable<FaceEncoding>)bf.Deserialize(stream);
-                        bool flag = false;
+
+                        //Face distances
+
+                        Console.WriteLine("-----------------------Face Distances-----------------------");
 
                         foreach (var encoding in facesEncodings)
-                            foreach(var compareFace in FaceRecognition.CompareFaces(encodingImage, encoding))
+                            foreach(var compareFace in FaceRecognition.FaceDistances(encodingImage, encoding))
+                            {
+                                var pathName = file.Split('_');
+                                var name = pathName[0].ToString().Split('\\').Last();
+
+                                if (compareFace <= 0.55)
+                                    Console.WriteLine("Result: {0}, Name: {1}, IMG: {2}", compareFace, name, file);
+                                else
+                                    Console.WriteLine("Result: {0}, Name: Unknown, IMG: {1}", compareFace, file);
+                            }
+
+                        //Compare Faces
+
+                        Console.WriteLine("-----------------------Compare Faces-----------------------");
+
+                        foreach (var encoding in facesEncodings)
+                            foreach (var compareFace in FaceRecognition.CompareFaces(encodingImage, encoding, 0.55))
                             {
                                 var pathName = file.Split('_');
                                 var name = pathName[0].ToString().Split('\\').Last();
@@ -39,16 +60,12 @@ namespace FaceMatching.Services
                                     Console.WriteLine("Result: {0}, Name: {1}, IMG: {2}", compareFace, name, file);
                                 else
                                     Console.WriteLine("Result: {0}, Name: Unknown, IMG: {1}", compareFace, file);
-
-                                flag = true;
                             }
-
-                        if (!flag)
-                            Console.WriteLine("Result: False, Name: Unknown, IMG: {0}", file);
                     }
                 }
             }
             return true;
         }
+
     }
 }
